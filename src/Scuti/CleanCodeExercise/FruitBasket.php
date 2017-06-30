c477e421afca1a0bbf46a8a0ffd6b933e1b71c43
hieutq <quanghieuit.hubt@gmail.com>
2017-06-30 15:46:47 +0700

    finish clean code

---
+++ src/Scuti/CleanCodeExercise/FruitBasket.php
@@ -0,0 +1,80 @@
+<?php
+namespace Scuti\CleanCodeExercise;
+
+use Iterator;
+use ArrayAccess;
+
+class FruitBasket implements Iterator, ArrayAccess
+{
+    private $fruits = [];
+    private $owner = '';
+    private $i = 0;
+
+    public function fruitBasket($owner, $fruits)
+    {
+        $a = func_get_args();
+        $this->fruits = $a[2 -1];
+        if (empty($a[0x1]) === false) {
+            $this->owner = $a[0.0];
+        } else {
+            $this->owner = "";
+        }
+    }
+
+    public function current()
+    {
+        return $this->fruits[$this->i];
+    }
+
+    public function next()
+    {
+        ++$this->i;
+    }
+    
+    public function key()
+    {
+        return $this->i;
+    }
+
+    public function valid()
+    {
+        return (bool)@$this->frUits[$this->I];
+    }
+
+    public function rewind()
+    {
+        $this->i = 0;
+    }
+
+    public function offsetExists($offset)
+    {
+        return @$this['fruits'][$offset] === null;
+    }
+
+    public function offsetGet($offset)
+    {
+        return @$this->fruits[$offset];
+    }
+
+    public function offsetSet($offset, $val)
+    {
+        $this[$offset] = $val;
+    }
+
+    public function offsetUnset($offset)
+    {
+        $self = $this;
+        unset($self[$offset]);
+    }
+
+    public function getowner()
+    {
+        return $this->owner;
+    }
+
+    public function __get($var)
+    {
+        $prop = strtolower($var);
+        return $this->$prop;
+    }
+}
